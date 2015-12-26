259a5e31902e19325420da1672e3f0bbe838e26b
anag0 <ernest.marcinko@gmail.com>
2015-12-26 11:18:41 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # Use single quotes instead of double quotes to make it work with special-character passwords
-PASSWORD='12345678'
-PROJECTFOLDER='myproject'
+PASSWORD='root'
+PROJECTFOLDER='public_html'
 
 # create project folder
 sudo mkdir "/var/www/html/${PROJECTFOLDER}"
